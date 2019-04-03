f8ec6f9d8047d0d4a67b7e21acdaf155f3d64f02
vijaynailwal1992 <vijaynailwal92@gmail.com>
2019-04-03 21:46:03 +0530

    Initial commit

---
+++ app/src/main/java/com/vijay/customtoast/MainActivity.java
@@ -0,0 +1,88 @@
+package com.vijay.customtoast;
+
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.StyleSpan;
+import android.view.View;
+
+import static android.graphics.Typeface.BOLD_ITALIC;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_error_toast).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toasty.error(MainActivity.this, R.string.error_message, Toasty.LENGTH_SHORT, true).show();
+            }
+        });
+        findViewById(R.id.button_success_toast).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toasty.success(MainActivity.this, R.string.success_message, Toasty.LENGTH_SHORT, true).show();
+            }
+        });
+        findViewById(R.id.button_info_toast).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toasty.info(MainActivity.this, R.string.info_message, Toasty.LENGTH_SHORT, true).show();
+            }
+        });
+        findViewById(R.id.button_warning_toast).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toasty.warning(MainActivity.this, R.string.warning_message, Toasty.LENGTH_SHORT, true).show();
+            }
+        });
+        findViewById(R.id.button_normal_toast_wo_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toasty.normal(MainActivity.this, R.string.normal_message_without_icon).show();
+            }
+        });
+        findViewById(R.id.button_normal_toast_w_icon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Drawable icon = getResources().getDrawable(R.drawable.ic_pets_white_48dp);
+                Toasty.normal(MainActivity.this, R.string.normal_message_with_icon, icon).show();
+            }
+        });
+        findViewById(R.id.button_info_toast_with_formatting).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toasty.info(MainActivity.this, getFormattedMessage()).show();
+            }
+        });
+        findViewById(R.id.button_custom_config).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toasty.Config.getInstance()
+                        .setToastTypeface(Typeface.createFromAsset(getAssets(), "PCap Terminal.otf"))
+                        .allowQueue(false)
+                        .apply();
+                Toasty.custom(MainActivity.this, R.string.custom_message, getResources().getDrawable(R.drawable.laptop512),
+                        android.R.color.black, android.R.color.holo_green_light, Toasty.LENGTH_SHORT, true, true).show();
+                Toasty.Config.reset(); // Use this if you want to use the configuration above only once
+            }
+        });
+    }
+
+    private CharSequence getFormattedMessage() {
+        final String prefix = "Formatted ";
+        final String highlight = "bold italic";
+        final String suffix = " text";
+        SpannableStringBuilder ssb = new SpannableStringBuilder(prefix).append(highlight).append(suffix);
+        int prefixLen = prefix.length();
+        ssb.setSpan(new StyleSpan(BOLD_ITALIC),
+                prefixLen, prefixLen + highlight.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return ssb;
+    }
+}
---
+++ app/src/main/java/com/vijay/customtoast/ToastyUtils.java
@@ -0,0 +1,46 @@
+package com.vijay.customtoast;
+
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.NinePatchDrawable;
+import android.os.Build;
+import android.support.annotation.ColorInt;
+import android.support.annotation.ColorRes;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.content.res.AppCompatResources;
+import android.view.View;
+
+
+
+final class ToastyUtils {
+    private ToastyUtils() {
+    }
+
+    static Drawable tintIcon(@NonNull Drawable drawable, @ColorInt int tintColor) {
+        drawable.setColorFilter(tintColor, PorterDuff.Mode.SRC_IN);
+        return drawable;
+    }
+
+    static Drawable tint9PatchDrawableFrame(@NonNull Context context, @ColorInt int tintColor) {
+        final NinePatchDrawable toastDrawable = (NinePatchDrawable) getDrawable(context, R.mipmap.toast_frame);
+        return tintIcon(toastDrawable, tintColor);
+    }
+
+    static void setBackground(@NonNull View view, Drawable drawable) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+            view.setBackground(drawable);
+        else
+            view.setBackgroundDrawable(drawable);
+    }
+
+    static Drawable getDrawable(@NonNull Context context, @DrawableRes int id) {
+        return AppCompatResources.getDrawable(context, id);
+    }
+
+    static int getColor(@NonNull Context context, @ColorRes int color){
+        return ContextCompat.getColor(context, color);
+    }
+}
